ecbdbff4bbf835f5faa32857daf9b15f84a48d8a
ManueleBaracchi <baracchi.manuele@einaudicorreggio.it>
2023-04-17 19:35:32 +0200

    Aggiunta la ricerca per periodo singolo

--- Struttura/CalcolaEntrate.java
+++ Struttura/CalcolaEntrate.java
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 package Struttura;
 
 import Classe_Conto.Conto;
@@ -22,28 +22,4 @@ class CalcolaEntrate{
     }
 
 }
-=======
-package Struttura;
 
-import Classe_Conto.Conto;
-
-import java.util.ArrayList;
-
-class CalcolaEntrate{
-    ArrayList<Conto> calcolo;
-    CalcolaEntrate(ArrayList<Conto> c){
-        calcolo=c;
-    }
-
-    int calcolatore()
-    {
-        int totale=0;
-
-        for (Conto c:calcolo) {
-            totale += c.getAmmontare();
-        }
-        return totale;
-    }
-
-}
->>>>>>> origin/main
